854c0eb29cd9ba1297d537719a4da87a34327362
Srushttee <sendtosrushtitatte66@gmail.com>
2025-01-09 13:07:39 +0530

    Third Update

---
+++ Javascript-main/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3 + 3);
+
+console.log("Srushti");
+
+let name="Srushti" //String datatype
+let age=21
+let isLoggedIn =false
+let state = null
+
+console.log(typeof "Srushti") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
