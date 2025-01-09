dcc96b8cf7eed408244ad6cf277c266f2cdfabbb
Srushttee <sendtosrushtitatte66@gmail.com>
2025-01-09 12:48:47 +0530

    Second Update

---
+++ Javascript-main/Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail="srushtitatte@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+console.log(accountId);
+accountEmail="abab@gmail.com"
+accountPassword="121234"
+accountCity="Pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
