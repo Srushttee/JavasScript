3d31166ec934424c70d35b5aea84e6f91b1759eb
Srushttee <sendtosrushtitatte66@gmail.com>
2025-01-09 13:28:29 +0530

    Fouth Update

---
+++ conversionoperations.js
@@ -0,0 +1,14 @@
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn="Srushti"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
